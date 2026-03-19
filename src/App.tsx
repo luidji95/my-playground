84e33e3f86ab94a981ddd7253d7a71c27af13203
Milos Petrovic <milos.petrovic09.95@gmail.com>
2026-03-19 22:19:50 +0100

    var let const

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,21 @@ import './App.css'
 
 function App() {
 
-  console.log(x);
-  const x = 6;
 
+// PRIMER 1
+console.log(x); // undefined
+var x = 5;
+----------------------------------------
+//PRIMER 2
+console.log(y); // ReferenceError - TDZ
+let y = 10;
+----------------------------------------
+console.log(y); // ReferenceError - TDZ
+let Z = 10;
 
-
+const user = { name: 'Milos' };
+user.name = 'Petar'; //OK
+----------------------------------------
   return (
     <>
       <section className='my-plg'>
@@ -26,9 +36,18 @@ export default App
 
 /* 
 
-Razlika izmedju var let i const je u tome sto 
+Razlika izmedju var let i const 
 
-var - ima funkcijski scope 
+var → funkcijski scope, hoistuje se kao undefined => PRIMER 1
+let → blok scope, ima TDZ, ne može pre deklaracije => PRIMER 2
+const → isto kao let + ne može reassign (ali može mutirati objekat) => PRIMER 3
+
+TDZ (Temporal Dead Zone):
+Vreme od hoistovanja do deklaracije — pristup u tom periodu baca grešku
+
+JavaScript je:
+- Single-threaded
+- Synchronous by default
 
 
 
