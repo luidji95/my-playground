075f3656ebf0548fca9bbcc135a6b7d053720f69
Milos Petrovic <milos.petrovic09.95@gmail.com>
2026-03-19 22:18:26 +0100

    var let const

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,32 @@ import './App.css'
 
 function App() {
 
+  console.log(x);
+  const x = 6;
+
+
 
   return (
     <>
       <section className='my-plg'>
         <h1>Lets play!</h1>
+       
+        {}
       </section>
     </>
   )
 }
 
 export default App
+
+
+
+/* 
+
+Razlika izmedju var let i const je u tome sto 
+
+var - ima funkcijski scope 
+
+
+
+*/
