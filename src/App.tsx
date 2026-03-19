fbf0a6bdaee076a86f376e9d687423c5c1bd84fc
Milos Petrovic <milos.petrovic09.95@gmail.com>
2026-03-19 02:43:40 +0100

    let's play

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <section className='my-plg'>
+        <h1>Lets play!</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
